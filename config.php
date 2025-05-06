93b4dcbf64db2ead69dd6ef030eeac350471a8a2
CHEELEONG_KHONG <khongcl1307@gmail.com>
2025-05-06 22:57:22 +0800

    changed database name in  config

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "web_traffic_analysis";
+$database = "trafanalyz";
 
 
 $conn = new mysqli($host, $username, $password, $database);
