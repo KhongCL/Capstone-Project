2f13d042edf639abd52be26cad94477aa1368b49
iKynax <kynax2408@gmail.com>
2025-05-21 00:19:34 +0800

    tested security measures

--- auth/security.php
+++ auth/security.php
@@ -13,4 +13,12 @@ function sanitizeOutput($data) {
 function validateUsername($username) {
     return preg_match('/^[a-zA-Z0-9_]{5,20}$/', $username);
 }
+
+function validateEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function validatePassword($password) {
+    return preg_match('/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/', $password);
+}
 ?>
