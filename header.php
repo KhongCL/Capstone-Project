48011930bc73a3b435232dcd7ac96a50c74d48c4
Mervin Ooi <mervinooi221@gmail.com>
2025-07-11 01:05:52 +0800

    fix wrong url

--- header.php
+++ header.php
@@ -36,11 +36,11 @@ $active_page = isset($active_page) ? $active_page : 'home';
             <ul>
                 <?php if (isset($_SESSION['user_id'])): ?>
                     <?php if ($_SESSION['role'] === 'Admin'): ?>
-                        <li><a href="/trafanalyz/admin/index.php">Admin Dashboard</a></li>
+                        <li><a href="/admin/index.php">Admin Dashboard</a></li>
                     <?php else: ?>
-                        <li><a href="/trafanalyz/user/index.php">Dashboard</a></li>
+                        <li><a href="/user/index.php">Dashboard</a></li>
                     <?php endif; ?>
-                    <li><a href="/trafanalyz/logout.php">Logout</a></li>
+                    <li><a href="/logout.php">Logout</a></li>
                 <?php else: ?>
                     <li><a href="login.php">Login</a></li>
                     <li><a href="register.php">Register</a></li>
